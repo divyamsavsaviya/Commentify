6afa2e1df2d292629880cdbb99d8a9d0de90c7a4
Divyam Savsaviya <59193532+divyamsavsaviya@users.noreply.github.com>
2023-06-04 00:20:50 +0530

    Added dummy file to test the extension

---
+++ src/TestFiles/dummy.js
@@ -0,0 +1,16 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+// Test cases
+console.log(addNumbers(2, 3)); // Output: 5
+console.log(multiplyNumbers(4, 5)); // Output: 20
+console.log(greet("John")); // Output: Hello, John!
