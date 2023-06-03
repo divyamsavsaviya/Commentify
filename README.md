deffafff603a2e38d859623db9c0d3c101f4a370
Divyam Savsaviya <59193532+divyamsavsaviya@users.noreply.github.com>
2023-06-04 00:02:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Commentify
+
+Commentify is a Visual Studio Code extension that automates the process of adding informative and professional comments above your functions using AI-generated content. It enhances code readability, collaboration, and saves valuable development time.
+
+![Commentify in Action](commentify-demo.gif)
+
+## Features
+
+- Automatically adds comments above JavaScript and JavaScript React functions.
+- Utilizes the power of ChatGPT to generate informative comments based on the function name and body.
+- Improves code documentation and enhances collaboration among developers.
+- Works seamlessly with any JavaScript project in Visual Studio Code.
+
+## Installation
+
+1. Launch Visual Studio Code.
+2. Go to the Extensions view (`Ctrl+Shift+X` or `Cmd+Shift+X`).
+3. Search for "Commentify" in the Marketplace.
+4. Click "Install" to install the extension.
+5. Reload Visual Studio Code to activate the extension.
+
+## Usage
+
+1. Open a JavaScript or JavaScript React file in Visual Studio Code.
+2. Ensure the file contains functions that you want to add comments to.
+3. Run the "Add Comments Above Functions" command by:
+   - Opening the Command Palette (`Ctrl+Shift+P` or `Cmd+Shift+P`).
+   - Searching for "Add Comments Above Functions".
+   - Selecting the command from the list or using the specified keybinding.
+4. Commentify will automatically add comments above the functions using AI-generated content.
+
+## Configuration
+
+Commentify currently doesn't require any additional configuration. It utilizes the default settings to generate comments. However, stay tuned for future updates that may include configurable options.
+
+## Troubleshooting
+
+If you encounter any issues or have questions, you can reach out to us by creating an issue in the [Commentify GitHub repository](https://github.com/your-username/commentify).
+
+## Contributions
+
+Contributions are welcome! If you would like to contribute to Commentify, please follow the guidelines in the [CONTRIBUTING.md](CONTRIBUTING.md) file.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
---
+++ src/extension.ts
@@ -0,0 +1,68 @@
+
+import * as vscode from 'vscode';
+import { window, workspace, commands, ExtensionContext } from 'vscode';
+import axios from 'axios';
+
+async function addCommentsAboveFunctions() {
+    const editor = window.activeTextEditor;
+    if (editor) {
+        const document = editor.document;
+        const languageId = document.languageId;
+        if (languageId === 'javascript' || languageId === 'javascriptreact') {
+            const functionRegex = /function\s+(\w+)\s*\([^)]*\)\s*{([\s\S]*?)}\s*(?=function|$)/g;
+            const functionComments: { [key: string]: string } = {}; // Define functionComments as a dictionary
+            let match;
+            while ((match = functionRegex.exec(document.getText()))) {
+                const functionName = match[1];
+                const functionBody = match[2];
+                if (!functionComments.hasOwnProperty(functionName)) {
+                    functionComments[functionName] = ''; // Initialize comment as an empty string
+                }
+                if (functionComments[functionName] === '') {
+                    // Get comment for the function from ChatGPT
+                    const comment = await getCommentFromChatGPT(functionName, functionBody); // Use await to wait for the promise to resolve
+                    functionComments[functionName] = comment;
+                }
+            }
+
+            // Insert comments above the functions
+            editor.edit(editBuilder => {
+                for (const functionName in functionComments) {
+                    const comment = functionComments[functionName];
+                    const functionPosition = document.positionAt(document.getText().indexOf(functionName));
+                    const commentText = `/**\n * ${comment}\n */\n`;
+                    editBuilder.insert(functionPosition.with({ character: 0 }), commentText);
+                }
+            });
+        }
+    }
+}
+
+async function getCommentFromChatGPT(functionName: string, functionBody: string): Promise<string> {
+    try {
+        const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+            prompt: `Function: ${functionName}\n\nBody: ${functionBody}\n\nDescription: `,
+            max_tokens: 50,
+            temperature: 0.7,
+        }, {
+            headers: {
+                'Authorization': 'Bearer YOUR_API_KEY',
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const comment = response.data.choices[0].text.trim();
+        return comment;
+    } catch (error) {
+        console.error('Failed to get comment from ChatGPT:', error);
+        return '';
+    }
+}
+
+
+export function activate(context: ExtensionContext) {
+    let disposable = commands.registerCommand('extension.addCommentsAboveFunctions', addCommentsAboveFunctions);
+
+    context.subscriptions.push(disposable);
+}
+
